a3d2f4870d481d7d36c2b8d4e25f72b6fa3c2cbc
Vinnie Monaco <vinmonaco@gmail.com>
2015-05-07 09:53:49 -0400

    fitfrail in progress, updated readme

--- R/fitfrail.R
+++ R/fitfrail.R
@@ -48,18 +48,19 @@ fitfrail <- function(data) {
     sum(data$status[data$time == time_steps[k]])
   })
   
+  # Create psi_i for each family. Depends on theta, t, and H_i.(t)
   psi_ <- sapply(family_names, function (i) {
-    function(theta, k, H_ik) {
+    function(theta, k, H_dot_ik) {
       
       phi_1 <- function (w) {
-        ( w ^ N_[[i]][k] ) * exp(-w*H_ik) * dgamma(w, theta, theta)
+        ( w ^ N_[[i]][k] ) * exp(-w*H_dot_ik) * dgamma(w, theta, theta)
       }
       
       phi_2 <- function (w) {
-        ( w ^ (N_[[i]][k] + 1) ) * exp(-w*H_ik) * dgamma(w, theta, theta)
+        ( w ^ (N_[[i]][k] + 1) ) * exp(-w*H_dot_ik) * dgamma(w, theta, theta)
       }
       
-      integrate(phi_2, -Inf, Inf)$value/integrate(phi_1, -Inf, Inf)$value
+      integrate(phi_2, 0, Inf)$value/integrate(phi_1, 0, Inf)$value
     }
   }, simplify = FALSE, USE.NAMES = TRUE)
   
@@ -78,17 +79,9 @@ fitfrail <- function(data) {
     }, simplify = FALSE, USE.NAMES = TRUE)
     
     for (k in 2:length(time_steps)) {
-      # Determine H_ij(t) and H_i.(t)
-      for (i in family_names) {
-        for (j in 1:nrow(families[[i]])) {
-          k_min = min(families[[i]][j,"rank"], k)
-          H_[[i]][j, k] <- lambda_hat[k_min] * exp(t(beta) %*% families[[i]][j,covars])
-        }
-        H_dot[[i]][k] <- sum(H_[[i]][,k])
-      }
-      
+      # Denom for delta_lambda_hat[k]
       denom <- sum(sapply(family_names, function(i) {
-          psi_[[i]](theta, k, H_[[i]][k]) * 
+          psi_[[i]](theta, k, H_dot[[i]][k]) * 
           sum(sapply(1:nrow(families[[i]]), function(j) {
             Y_[[i]][j, k] * exp(t(beta) %*% families[[i]][j,covars])
           }))
@@ -97,15 +90,61 @@ fitfrail <- function(data) {
       delta_lambda_hat[k] <- d_[k]/denom
     
       # Update the cumsum since it's needed several times in the next iteration
-      lambda_hat[k] <- cumsum(delta_lambda_hat[1:k])
+      lambda_hat[k] <- sum(delta_lambda_hat[1:k])
+      
+      # Determine H_ij(t) and H_i.(t)
+      for (i in family_names) {
+        for (j in 1:nrow(families[[i]])) {
+          k_min = min(families[[i]][j,"rank"], k)
+          H_[[i]][j, k] <- lambda_hat[k_min] * exp(t(beta) %*% families[[i]][j,covars])
+        }
+        H_dot[[i]][k] <- sum(H_[[i]][,k])
+      }
     }
     
-    # The estimator of the cumulative baseline hazard
-    lambda_hat <- cumsum(delta_lambda_hat)
-    
-    list(lambda_hat, H_, H_dot)
+    list(lambda_hat=lambda_hat, H_=H_, H_dot=H_dot)
   }
   
-  estimate_lambda_hat
+  # TODO: Build the system of equations
+  # E.g. will look something like this, without the duplicate integrations
+  U_r <- function(beta, theta, covarcol) {
+    estimator <- estimate_lambda_hat(beta, theta)
+    lambda_hat <- estimator$lambda_hat
+    H_ <- estimator$H_
+    H_dot <- estimator$H_dot
+    
+    tau <- length(time_steps)
+    term1 <- sum(data[,statuscol]*data[,covarcol])
+    
+    term2 <- sum(sapply(family_names, function(i) {
+      psi_[[i]](theta, tau, H_dot[[i]][tau]) * 
+        sum(sapply(1:nrow(families[[i]]), function(j) {
+          H_[[i]][j, families[[i]][j,"rank"]] * families[[i]][j,covarcol]
+        }))
+    }))
+    
+    term1 - term2
+  }
+  
+  U_p <- function(beta, theta) {
+    estimator <- estimate_lambda_hat(beta, theta)
+    lambda_hat <- estimator$lambda_hat
+    H_ <- estimator$H_
+    H_dot <- estimator$H_dot
+    
+    tau <- length(time_steps)
+    
+    sum(sapply(family_names, function(i) {
+      integrate(function (w) {
+        # TODO: df(w)/dtheta correct? Getting a divergent integral with large thete right now.
+        ( w ^ N_[[i]][tau] ) * exp(-w*H_[[i]][tau]) * sapply(w, function (wi) {grad(function(p) {dgamma(wi, 1/p, 1/p)}, x=theta)})
+      }, 0, Inf)$value /
+        integrate(function (w) {
+        ( w ^ N_[[i]][tau] ) * exp(-w*H_[[i]][tau]) * dgamma(w, 1/theta, 1/theta)
+      }, 0, Inf)$value
+    }))
+  }
+  
+  U_r
 }
 
