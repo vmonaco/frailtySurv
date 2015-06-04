9c9463f0cd636ed2b0e1738fb3d056dedc9bf830
Vinnie Monaco <vinmonaco@gmail.com>
2015-06-04 13:08:27 -0400

    Updated fitfrail docs

--- R/fitfrail.R
+++ R/fitfrail.R
@@ -1,17 +1,5 @@
-#' Fit a Shared Frailty model
-#'
-#' \code{fitfrail} fits a model.
-#'
-#' This function ...
-#' 
-#' @param ... model
-#' @return parameters
-#'   \url{} for more details.
-#' @examples
-#' fitfrail(genfrail())
-#' 
-#' @export
-fitfrail <- function(formula, data, control, frailty=c("gamma","lognormal","posstab"), ...) {
+fitfrail <- function(formula, data, control, 
+                     frailty=c("gamma","lognormal","posstab"), ...) {
   
   Call <- match.call()
   
