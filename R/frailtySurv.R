19d8dfeb6cbdba22d2e0f3679de7d31f80bebd2c
Vinnie Monaco <contact@vmonaco.com>
2018-06-08 11:14:19 -0400

    Updated version 1.3.4

--- R/frailtySurv.R
+++ R/frailtySurv.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Welcome to frailtySurv v1.3.3")
+  packageStartupMessage("Welcome to frailtySurv v1.3.4")
 }
 
 .onLoad <- function(libname, pkgname) {
